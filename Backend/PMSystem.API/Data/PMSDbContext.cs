c8c29fee3f87c3c4339ec357822803569a66e7d8
adisskender01 <112349156+adisskender01@users.noreply.github.com>
2023-09-29 23:04:28 +0200

    Add files via upload

---
+++ Backend/PMSystem.API/Data/PMSDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PMSystem.API.Models;
+
+namespace PMSystem.API.Data
+{
+    public class PMSDbContext : DbContext
+    {
+        public PMSDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
